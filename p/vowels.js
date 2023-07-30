af75e3f0f450d870021a0aa3d01508450867bde6
Larry Battle <larry.battle.tech@gmail.com>
2023-07-30 16:36:08 -0500

    Create vowels.js

---
+++ p/vowels.js
@@ -0,0 +1,67 @@
+// date created : July 30, 2023
+
+[
+  {
+    "letter": "a",
+    "shortVowel": ["cat", "mat", "rat", "hat", "bat"],
+    "longVowel": ["late", "rate", "mate", "hate", "bate"]
+  },
+  {
+    "letter": "e",
+    "shortVowel": ["bet", "pet", "met", "let", "net"],
+    "longVowel": ["meet", "beat", "feat", "neat", "peat"]
+  },
+  {
+    "letter": "i",
+    "shortVowel": ["hit", "sit", "bit", "kit", "pit"],
+    "longVowel": ["lite", "site", "bite", "kite", "pite"]
+  },
+  {
+    "letter": "o",
+    "shortVowel": ["hot", "lot", "pot", "not", "dot"],
+    "longVowel": ["note", "vote", "rote", "dote", "mote"]
+  },
+  {
+    "letter": "u",
+    "shortVowel": ["cut", "hut", "but", "nut", "rut"],
+    "longVowel": ["mute", "cute", "flute", "brute", "lute"]
+  },
+  {
+    "letter": "y",
+    "shortVowel": ["myth", "gym", "sylph", "sync", "syndrome"],
+    "longVowel": ["type", "hype", "ripe", "dye", "tyke"]
+  }
+]
+// ===
+[
+  {
+    "letter": "a",
+    "shortVowel": ["cat", "bat", "hat", "rat", "man"],
+    "longVowel": ["day", "may", "say", "way", "lay"]
+  },
+  {
+    "letter": "e",
+    "shortVowel": ["pen", "men", "bed", "red", "hen"],
+    "longVowel": ["he", "me", "we", "she", "be"]
+  },
+  {
+    "letter": "i",
+    "shortVowel": ["it", "is", "his", "sit", "hit"],
+    "longVowel": ["I", "my", "why", "lie", "tie"]
+  },
+  {
+    "letter": "o",
+    "shortVowel": ["hot", "lot", "not", "pot", "got"],
+    "longVowel": ["go", "no", "so", "show", "old"]
+  },
+  {
+    "letter": "u",
+    "shortVowel": ["us", "up", "but", "run", "sun"],
+    "longVowel": ["use", "cube", "mule", "rude", "tune"]
+  },
+  {
+    "letter": "y",
+    "shortVowel": ["my", "by", "try", "cry", "fly"],
+    "longVowel": ["my", "by", "try", "cry", "fly"]
+  }
+]
